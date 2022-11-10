0641becd81340bb5b930f1b87085cc7f10b551a6
潘肖男 <panxiaonan@enn.cn>
2022-11-10 19:19:14 +0800

    路线纠偏算法

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# route-correction
-路线纠偏算法
+## Point 坐标点类
+| 字段 | 类型 | 描述 |
+|-----|------|-----|
+| lng | Number | 经度 |
+| lat | Number | 纬度 |
+
+## Segment 路径线段类
+| 字段 | 类型 | 描述 |
+|-----|------|-----|
+| p0 | Point | 起始点 |
+| p1 | Point | 结束点 |
+| length | Number | 路径线段长度（米） |
+| distance | Number | p0点距离路径起始点的距离 |
+| direction | Number | 路径朝向（弧度） |
+| directionInDegree | Number | 路径朝向（角度） |
+
+## Route 路线类
+| 字段 | 类型 | 描述 |
+|-----|------|-----|
+| points | [Point] | 折线点 |
+| segments | [Segment] | 路径线段 |
+| totalDistance | Number | 总长度(米) |
+| maxLng / minLng | Number | 最大最小经度 |
+| maxLat / minLat | Number | 最大最小纬度 |、
+| centerLng / centerLat | Number | 包围盒中心经纬度 |
+| RADIUS | Number | 地球半径（包围盒做梯形正畸后近似） |
+### 成员方法 
+#### nearest 方法
+`const res = nearest(point);`
+##### 入参
+| 字段 | 类型 | 描述 |
+|-----|------|-----|
+| point | Point | 计算 point 点到当前路径的最近点位 |
+##### 返回值
+返回值 res 为一个对象，其中各属性如下
+| 字段 | 类型 | 描述 |
+|-----|------|-----|
+| point | Point | 最近点 |
+| point.r | Number | 最近点在线段上的位置，范围 0~1。0 代表最近点位于 p0，1 代表最近点位于 p1, 0.5 代表位于线段中点 |
+| point.lng / point.lat | Number | 最近点的经纬度 |
+| point.distance | Number | 最近点距离线段 p0 点的距离（米） |
+| segment | Segment | 最近点所在线段 |
+| index | Number | 最近点所在线段在 segments 数组中的下标 |
+| distance | Number | 最近点距离路径起始点的距离（米） |
---
+++ route.js
@@ -0,0 +1,162 @@
+export default class Route {
+  constructor(points, opt = {}) {
+    this.points = points;
+    this.opt = opt || {};
+
+    if(!points.length) return;
+    this.maxLng = points[0].lng;
+    this.minLng = points[0].lng;
+    this.maxLat = points[0].lat;
+    this.minLat = points[0].lat;
+
+    // 计算经纬度最大最小值
+    for(let i = 1; i < points.length; i++) {
+      const p = points[i];
+      if (p.lng > this.maxLng) {
+        this.maxLng = p.lng;
+      }
+      if (p.lng < this.minLng) {
+        this.minLng = p.lng;
+      }
+      if (p.lat > this.maxlat) {
+        this.maxlat = p.lat;
+      }
+      if (p.lat < this.minLat) {
+        this.minLat = p.lat;
+      }
+    }
+    // 包围盒中心点经纬度
+    this.centerLng = (this.maxLng + this.minLng) / 2;
+    this.centerLat = (this.maxLat + this.minLat) / 2;
+    // 梯形正畸参数
+    this.latCos = Math.cos( this.centerLat / 180 * Math.PI );
+    this.latCos2 = this.latCos ** 2;
+    this.latSin = Math.sin( this.centerLat / 180 * Math.PI );
+    this.latSin2 = this.latSin ** 2;
+    // 梯形正畸后近似地球半径
+    this.RADIUS = ((6377830 /* 赤道半径 */ ** 2 * this.latCos2)
+      + (6356908.8 /* 极半径 */ ** 2 * this.latSin2)) ** .5;
+
+    if(points.length <= 1) return;
+    this.segments = Array(points.length - 1);
+    let distance = 0;
+    for(let i = 0; i < points.length - 1; i++) {
+      const p0 = this.points[i];
+      const p1 = this.points[i + 1];
+      const length = this.distanceBetweenPoints(p0, p1);
+      // 计算路径朝向
+      let direction = null;
+      const dLng = p1.lng - p0.lng;
+      const dLat = p1.lat - p0.lat;
+      if(!dLng) { // 正北或正南
+        if(dLat > 0) { // 正北
+          direction = Math.PI / 2;
+        } else if (dLat === 0) { //两点相同，错误情况
+          direction = 0;
+        } else { // dLat < 0，正南
+          direction = - Math.PI / 2;
+        }
+      } else {
+        direction = Math.atan(dLat / dLng);
+        // 弧度角校准到 0~2 PI
+        if(dLng < 0) { // 单位向量在第二第三象限的情况
+          direction += Math.PI;
+        }
+        if(direction < 0) { // 单位向量在第四象限的情况
+          direction += Math.PI;
+        }
+      }
+      this.segments[i] = {
+        p0, p1, // 线段两端点
+        length, // 路径片段长度（米）
+        distance, // 距离路径起点的距离（米）
+        direction, // 路径朝向（弧度）
+        directionInDegree: direction * 180 / Math.PI, // 路径朝向（角度）
+      };
+      distance += length;
+    }
+    this.totalDistance = distance;
+  }
+
+  // 两点间距离（米），自带计算方法
+  distanceBetween(p0, p1) {
+    const dx2 = (p1.lng - p0.lng) ** 2 * this.latCos2;
+    const dy2 = (p1.lat - p0.lat) ** 2;
+    return ((dx2 + dy2) ** .5) / 180 * Math.PI * this.RADIUS;
+  }
+  // 两点间距离（米），可接受自定义方法
+  distanceBetweenPoints(p0, p1) {
+    if(this.opt.distanceFunc) {
+      return this.opt.distanceFunc(p0, p1);
+    } else {
+      return this.distanceBetween(p0, p1);
+    }
+  }
+
+  // 点到线段的最近距离（角度平方）
+  degDistance2ToSegment(point, segment) {
+    const { lng: x, lat: y } = point;
+    const {
+      p0: { lng: x0, lat: y0 },
+      p1: { lng: x1, lat: y1 },
+    } = segment;
+    let nearest = null;
+    let r = 0;
+
+    const cross = (x1 - x0) * (x - x0) * this.latCos2 + (y1 - y0) * (y - y0);
+    if(cross < 0) {
+      // 最近点为 p0
+      nearest = { x: x0, y: y0 }
+    } else {
+      const d2 = (x1 - x0) ** 2 * this.latCos2 + (y1 - y0) ** 2;
+      if(cross >= d2) {
+        // 最近点为 p1
+        nearest = { x: x1, y: y1 };
+        r = 1;
+      } else {
+        // 最近在 p0 至 p1 之间，坐标为 (px, py)
+        r = cross / d2; // 距离 p0 的比例
+        const px = x0 + (x1 - x0) * r;
+        const py = y0 + (y1 - y0) * r;
+        nearest = { x: px, y: py }
+      }
+    }
+
+    const dis2 = ((x - nearest.x) ** 2 * this.latCos2 + (y - nearest.y) ** 2);
+    return {
+      r, // 距离 p0 的比例
+      nearest, // 最近点
+      dis2, // 最近点的距离（角度的平方）
+    };
+  }
+
+  nearestBy(point) {
+    let minDis2 = Number.MAX_VALUE; // 
+    let index = 0;
+    let minDisRes = null;
+    // 根据 dis2 最小值，筛选出最近的点
+    for(let i = 0; i < this.segments.length; i++) {
+      const disRes = this.degDistance2ToSegment(point, this.segments[i]);
+      console.log()
+      if(disRes.dis2 < minDis2) {
+        minDis2 = disRes.dis2;
+        index = i;
+        minDisRes = disRes;
+      }
+    }
+
+    const segment = this.segments[index]
+    const distance = minDisRes.r * segment.length;
+    return {
+      index,
+      segment,
+      point: {
+        r: minDisRes.r,
+        distance,
+        lng: minDisRes.nearest.x,
+        lat: minDisRes.nearest.y,
+      },
+      distance: segment.distance + distance,
+    };
+  }
+}
